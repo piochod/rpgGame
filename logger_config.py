13ca450e287e2d04dad24e8ca40ddf0aaf1b0524
piochod <piochod@wp.pl>
2026-06-07 18:01:18 +0200

    format logger

--- logger_config.py
+++ logger_config.py
@@ -6,7 +6,7 @@ def get_logger(module_name: str) -> logging.Logger:
     """Sets up the universal logging configuration and returns a logger instance."""
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s | %(levelname)-8s | %(name)-12s | %(message)s',
+        format='[%(asctime)s][%(levelname)s]%(message)s',
         datefmt='%H:%M:%S'
     )
     return logging.getLogger(module_name)
